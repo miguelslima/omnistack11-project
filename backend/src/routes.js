a2564ba19718a95665f7bbb3230e6e505cfb9840
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-24 23:12:40 -0300

    Begin controller ongs and incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidetnsController = require('./controllers/IncidentController');
+
 const routes = express.Router();
 
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-routes.get('/users', (request, response) => {
-  return response.json({ 
-    evento: 'Semana Omnistack 11.0',
-    aluno: 'Miguel Lima'
-  });
-})
+routes.get('/incidents', IncidetnsController.index);
+routes.post('/incidents', IncidetnsController.create);
+routes.delete('/incidents/:id', IncidetnsController.delete);
 
 module.exports = routes;
