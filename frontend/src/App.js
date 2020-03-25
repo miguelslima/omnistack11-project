9d1a4f830416797326d276c20396cce1f0463428
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-25 09:51:07 -0300

    Introduction in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Header';
 
 function App() {
+  let counter = 0;
+
+  function increment() {
+    counter++;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header> Contador: {counter} </Header>
+      <button onClick={increment}> Incrementar </button>
     </div>
   );
 }
